150afa0d1e6efca955632411ac51ce6fc7579b19
Renz Lendio <131181504+O-rensa@users.noreply.github.com>
2024-10-27 18:36:08 +0800

    Added Timeouts

--- zig_tcp_server/server.zig
+++ zig_tcp_server/server.zig
@@ -3,7 +3,9 @@ const net = std.net;
 const posix = std.posix;
 
 pub fn main() !void {
-    const addr = try net.Address.parseIp4("127.0.0.1", 4000);
+    const ipHost: []const u8 = "192.168.1.145";
+    const portNum: u16 = 4000;
+    const addr = try net.Address.parseIp4(ipHost, portNum);
     const tpe: u32 = posix.SOCK.STREAM;
     const protoc = posix.IPPROTO.TCP;
 
@@ -14,7 +16,9 @@ pub fn main() !void {
     try posix.bind(listener, &addr.any, addr.getOsSockLen());
     try posix.listen(listener, 128);
 
-    std.debug.print("listening to port: {}", .{4000});
+    std.debug.print("listening at {s} port: {any}\n", .{ ipHost, portNum });
+
+    var buf: [128]u8 = undefined;
     while (true) {
         var client_addr: net.Address = undefined;
         var client_addr_len: posix.socklen_t = @sizeOf(net.Address);
@@ -26,20 +30,28 @@ pub fn main() !void {
 
         defer posix.close(scket);
         std.debug.print("{} connected\n", .{client_addr});
+        const timeout = posix.timeval{ .tv_sec = 2, .tv_usec = 500_000 };
+        try posix.setsockopt(listener, posix.SOL.SOCKET, posix.SO.RCVTIMEO, &std.mem.toBytes(timeout));
+        try posix.setsockopt(listener, posix.SOL.SOCKET, posix.SO.SNDTIMEO, &std.mem.toBytes(timeout));
 
-        write(scket, "Hello (and goodbye)") catch |err| {
-            std.debug.print("error writing: {}\n", .{err});
-        };
-    }
-}
-
-fn write(socket: posix.socket_t, msg: []const u8) !void {
-    var pos: usize = 0;
-    while (pos < msg.len) {
-        const written = try posix.write(socket, msg[pos..]);
-        if (0 == written) {
-            return error.Closed;
+        // read the message from client
+        const strm = net.Stream{ .handle = scket };
+        const read = try strm.read(&buf);
+        if (0 == read) {
+            continue;
         }
-        pos += written;
+
+        try strm.writeAll(buf[0..read]);
     }
 }
+
+//fn write(socket: posix.socket_t, msg: []const u8) !void {
+//    var pos: usize = 0;
+//    while (pos < msg.len) {
+//        const written = try posix.write(socket, msg[pos..]);
+//        if (0 == written) {
+//            return error.Closed;
+//        }
+//        pos += written;
+//    }
+//}
